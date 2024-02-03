326cc1405ef17c3ad063a8c016a4b62af531c37a
Calebkillom <Calebkillom@gmail.com>
2024-02-03 22:44:28 +0300

    task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
-/**
- * dlistint_len - returns the number of elements in a linked dlistint_t list
- * @h: pointer variable to dlistint_t struct
- *
- * Return: number of elements in a linked dlistint_t list
- */
 
+/**
+ * dlistint_len - Returns the number of elements in a dlistint_t list
+ * @h: Pointer to the head of the dlistint_t list
+ *
+ * Return: Number of elements in the list
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t count;
-	const dlistint_t *current;
-
-	current = h;
+	size_t count = 0;
+	const dlistint_t *current = h;
 
 	while (current != NULL)
 	{
@@ -22,5 +20,4 @@ size_t dlistint_len(const dlistint_t *h)
 	}
 
 	return (count);
-
 }
